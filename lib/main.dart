0d3bacbd3d0be8dce2e0f312c68da84614e1205b
Brandon Sanchez <bsanch05@nyit.edu>
2025-10-09 12:34:38 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+void main(){
+  runApp(const MaterialApp(home: Expenses()));
+}
