fb3eed81e77c7fc1caa6ab7304f62416ec8e04a7
Brandon Sanchez <bsanch05@nyit.edu>
2025-10-14 11:53:48 -0400

    stubbed out Expenses Chart

--- lib/expenses_list/expenses_list.dart
+++ lib/expenses_list/expenses_list.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget{
   const ExpensesList({super.key, required this.expenses});
@@ -8,7 +9,7 @@ class ExpensesList extends StatelessWidget{
   Widget build(BuildContext context){
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
+      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
     );
   }
 }
