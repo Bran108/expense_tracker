81916da1d826129680b370e4bbb353b149bae2ad
Brandon Sanchez <bsanch05@nyit.edu>
2025-10-23 12:35:24 -0400

    finished date picker and started dropdown

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -1,5 +1,9 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
@@ -7,17 +11,31 @@ class NewExpense extends StatefulWidget{
   State<NewExpense> createState(){
     return _NewExpenseState();
   }
-
 }
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
   @override
   void dispose(){
     _titleController.dispose();
     _amountController.dispose();
     super.dispose();
+  }
+
+  void _presentDatePicker() async{
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+      );
+      setState((){
+        _selectedDate = pickedDate;
+      });
   }
 
 @override
@@ -44,24 +62,34 @@ Widget build(BuildContext context) {
             ),
           ),
               ),
-              const SizedBox(width:26),
+              const SizedBox(width:16),
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("Selected Date"),
+                    Text(
+                    _selectedDate == null 
+                      ? 'Select A Date'
+                      : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
-                      onPressed: (){}, 
+                      onPressed: _presentDatePicker, 
                      icon: const Icon(Icons.calendar_month),
-              ),
-              ],
-              ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),          
           Row(
             children: [
+              DropdownButton(
+                items: Category.values.map(
+                (category) => DropdownMenuItem(
+                  child: Text(category.name.toString(),),),).toList(),
+              onChanged: (value){},
+            ),
             Spacer(),
             ElevatedButton(onPressed:() {
               Navigator.pop(context);
